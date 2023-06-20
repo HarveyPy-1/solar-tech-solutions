ee6d603df0c5264279211b3cb9d9c594af33f2e1
salmist81 <salmist81@yahoo.com>
2023-06-20 14:55:06 +0100

    Revert "Progress so far. Continue with shading.html"

--- QUOTATION/script.js
+++
@@ -1,7 +0,0 @@
-function showWorkType(workType) {
-	// Store the selected work type in sessionStorage
-	sessionStorage.setItem("workType", workType);
-
-	// Redirect to the next page
-	window.location.href = "work-type.html";
-}
