4a75820c67570d600deab6d1dbf1144dd5d2f688
HarveyPy-1 <126327787+HarveyPy-1@users.noreply.github.com>
2023-06-21 20:58:28 +0100

    almost final

--- QUOTATION/script.js
+++ QUOTATION/script.js
@@ -5,3 +5,28 @@ function showWorkType(workType) {
 	// Redirect to the next page
 	window.location.href = "work-type.html";
 }
+
+
+function findAddress() {
+	var addressDiv = document.createElement("div");
+	addressDiv.textContent =
+		"Bathspa University \n Newton St Loe, Bath BA2 9BN";
+	document.getElementById("addressForm").appendChild(addressDiv);
+}
+
+
+
+const quotationForm = document.getElementById("addressForm");
+const quotationResult = document.getElementById("quotationResult");
+
+quotationForm.addEventListener("submit", function (event) {
+	event.preventDefault();
+
+	// Simulating form submission with a delay
+	setTimeout(function () {
+		// Hide the form
+		addressForm.style.display = "none";
+		// Show the quotation result
+		quotationResult.style.display = "block";
+	}, 2000);
+});
